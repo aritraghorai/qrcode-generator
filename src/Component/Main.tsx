cd0dd8dbd00306f7370d902ee21531bfbced9779
Aritra Ghorai <81803365+aritraghorai@users.noreply.github.com>
2022-06-08 18:27:29 +0530

    add github workflow

--- src/Component/Main.tsx
+++ src/Component/Main.tsx
@@ -6,7 +6,7 @@ const Main = () => {
   const handleClick = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     QRCode.toDataURL(url, {
-      width: 600,
+      width: 400,
       margin: 2,
     })
       .then((url) => {
@@ -28,28 +28,29 @@ const Main = () => {
             type="text"
             name=""
             id=""
-            className="border-2 rounded-md border-gray-400 p-1 w-[65vw]"
+            className="border-2 rounded-md border-gray-400 p-1 w-[65vw] "
             placeholder="https://aritraghorai.vercel.app/"
             onChange={(e) => {
               seturl(e.target.value);
             }}
+            required
           />
           <button
             type="submit"
-            className="ml-5 border-2 border-gray-300 rounded-md p-2 font-bold bg-[#1ea80c] text-white hover:bg-transparent hover:border-green-500 hover:text-black hover:border-1"
+            className="ml-5 border-2 border-gray-300 rounded-md p-2 font-bold bg-[#1ea80c] text-white hover:bg-transparent hover:border-green-500 hover:text-black hover:border-1 "
           >
-            Generate Qr
+            Generate
           </button>
         </form>
       </div>
-      <div className="flex flex-col justify-center items-center">
+      <div className="flex flex-col justify-center items-center m-3">
         {Qrcode && (
           <>
             <img src={Qrcode} alt="" className="" />
             <a
               href={Qrcode}
               download="qrcode.png "
-              className="ml-5 border-2 border-gray-300 rounded-md p-2 font-bold bg-red-500 text-white hover:bg-transparent hover:border-green-500 hover:text-black hover:border-1"
+              className="ml-5 border-2 border-gray-300 rounded-md p-2 font-bold bg-red-500 text-white hover:bg-transparent hover:border-green-500 hover:text-black hover:border-1 mt-2"
             >
               Download
             </a>
--- src/Component/Navbar.tsx
+++ src/Component/Navbar.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <div className="text-3xl flex justify-center items-center bg-gray-500 text-white ">
+    <div className="text-3xl flex justify-center items-center bg-gray-800 text-green-500 h-[50px]">
       Qr Code Generator
     </div>
   );
